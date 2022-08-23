329e1a5adc13255084bdaedf28080eefa57d5701
Hyunjoo Lee <alro92333@gmail.com>
2022-08-23 19:55:28 +0900

    🔨 Update seperate components css

--- 01-starting-setup/src/components/ExpenseDate.js
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -5,10 +5,10 @@ function ExpenseDate(props) {
     const year = props.date.getFullYear();
 
     return (
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__year'>{year}</div>
+            <div className='expense-date__day'>{day}</div>
         </div>
     );
 }
