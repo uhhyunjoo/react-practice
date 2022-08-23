314cf7435f1aaac62e04855cdede7072f4689a14
Hyunjoo Lee <alro92333@gmail.com>
2022-08-23 19:54:04 +0900

    🔨 Update seperate components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,7 +1,22 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+
 // Expense Item 데이터를 렌더링하는 컴포넌트
 
-function ExpenseItem(){
-    return <h2> Expense item! </h2>
+function ExpenseItem(props){
+    // helper constant (도우미 상수)
+    return (
+        <div className='expense-item'>
+            <ExpenseDate date = {props.date}></ExpenseDate>
+            <div className='expense_-item__description'>
+                <h2> {props.title} </h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    );
 }
-
+// App 컴포넌트에서 ExpenseItem으로 데이터를 전달하고
+// ExpenseItem  에서  props 를 통해 date, title, amount 등을 받고 데이터의 일부를 출력하고
+// ExpenseDate 에서 props 를 통해 props.date 를 전달 받아 더 안쪽에 있는 ExpensDate 컴포넌트로 보내진다.
+// props 는 A 컴포넌트에서 B 컴포넌트로 데이터를 전달하는 방식임!
 export default ExpenseItem;
