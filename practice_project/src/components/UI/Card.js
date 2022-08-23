04df70c8beddedfb7c18fa4937a9afad3de2e3be
Hyunjoo Lee <alro92333@gmail.com>
2022-08-23 21:38:26 +0900

    🔨 Update component things

--- practice_project/src/components/UI/Card.js
+++ practice_project/src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css'
 // Wrapper Component
-function Card(props){
+const Card = (props) => {
     const classes = 'card ' + props.className;
     // 이를 통해, 밖에서 className 으로 받는 것들이 문자열로 추가된다.
     return (
