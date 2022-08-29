a71433f01d103dd82e660f291f2a263b120dfac1
Hyunjoo Lee <alro92333@gmail.com>
2022-08-29 20:48:47 +0900

    🔨 Add title changeHandler as only one (...)

--- demo-project/src/components/NewExpense/ExpenseForm.js
+++ demo-project/src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    // document.getElementById('').addEventListener('click', (event) => {})
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+    // 하나의 컴포넌트에, 하나 이상의 상태를 가질 수 있다.
+    // 그리고 이걸 별도의 상태를 갖고 각각 업데이트 하고 관리할 수 있다.
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+    // 차이점 : 이 state 를 업데이트할 때마다, 한 개가 아닌 이 세 property 모두를 업데이트 해야 한다.
+
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+        // setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        })
+        // state 를 업데이트할 때 react 는 이것을 이전 state 와 병합하지 않는다.
+        // 따라서 다른 두 데이터를 잃어버리지 않도록 꼭 확인해야 한다.
+    };
+
+    const amountChangeHandler = (event) => {
+        // setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        })
+    };
+
+    const dateChangeHandler = (event) => {
+        // setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        })
     };
 
     return (
@@ -16,11 +50,11 @@ const ExpenseForm = () => {
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min='2021-01-01' max='2023-12-31' />
+                    <input type='date' min='2021-01-01' max='2023-12-31' onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
