395073bec06a89be9a138c9538bab1850eb22fd1
Hyunjoo Lee <alro92333@gmail.com>
2022-08-29 19:21:47 +0900

    🔨 Add initial useState files

--- demo-project/src/App.js
+++ demo-project/src/App.js
@@ -6,37 +6,37 @@ const App = () => {
       id: 'e1',
       title: '아이네',
       height: 158.00,
-      date: new Date(1994, 12, 17),
+      date: new Date(1994, 11, 17),
     },
     {
       id: 'e2', 
       title: '징버거',
       height: 161.9,
-      date: new Date(1995, 10, 8)
+      date: new Date(1995, 9, 8)
     },
     {
       id: 'e3',
       title: '릴파',
       height: 164,
-      date: new Date(1996, 3, 9),
+      date: new Date(1996, 2, 9),
     },
     {
       id: 'e4',
       title: '주르르',
       height: 161.9,
-      date: new Date(1997, 6, 10),
+      date: new Date(1997, 5, 10),
     },
     {
       id: 'e5',
       title: '고세구',
       height: 30000,
-      date: new Date(1998, 9, 9),
+      date: new Date(1998, 8, 9),
     },
     {
       id: 'e6',
       title: '비챤',
       height: 160,
-      date: new Date(2000, 1, 16),
+      date: new Date(2000, 0, 16),
     },
   ];
 
--- demo-project/src/components/Expenses/ExpenseItem.js
+++ demo-project/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,35 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 // Expense Item 데이터를 렌더링하는 컴포넌트
 
-function ExpenseItem(props){
-    // helper constant (도우미 상수)
+// Keep in mind : Your componenet is a 'function' which returns 'JSX'
+
+function ExpenseItem(props) {
+    let title = props.title; // variable -> clickHandler can change it
+
+    useState(props.title); // 원래 컴포넌트는 맨 처음에 한 번만 호출됨
+    // useState 는 특별한 변수를 생성해서, 해당 변수는 변경사항으로 인해 컴포넌트를 다시 호출되도록 함
+    // 초기값은 props.title 이고, 이걸 useState 에 인자로 전달함
+
+    const clickHandler = () => {
+        title = 'Updated!';
+        console.log(title);
+    };
+    // function clickHandler() {}
+
     return (
         <Card className='expense-item'>
-            <ExpenseDate date = {props.date}></ExpenseDate>
-            <h2> {props.title} </h2>
+            <ExpenseDate date={props.date}></ExpenseDate>
+
             <div className='expense_-item__description'>
+                <h2> {title} </h2>
                 <div className='expense-item__price'>{props.height}cm</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
@@ -21,3 +38,9 @@ function ExpenseItem(props){
 // ExpenseDate 에서 props 를 통해 props.date 를 전달 받아 더 안쪽에 있는 ExpensDate 컴포넌트로 보내진다.
 // props 는 A 컴포넌트에서 B 컴포넌트로 데이터를 전달하는 방식임!
 export default ExpenseItem;
+
+
+//document.getElementById('root').addEventListener()\
+
+// useState is 'react-hook'
+// all these hooks should be called directly inside of react componenet functions. (예외 존재하긴 한데, 지금은 패스 ㅇㅇ)
