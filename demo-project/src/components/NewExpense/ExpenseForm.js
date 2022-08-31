c1a9805be79cd795a4156fb7abd1199f601fb8f7
Hyunjoo Lee <alro92333@gmail.com>
2022-08-31 15:40:10 +0900

    🔨 Add submitHandler

--- demo-project/src/components/NewExpense/ExpenseForm.js
+++ demo-project/src/components/NewExpense/ExpenseForm.js
@@ -20,8 +20,21 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault(); // 기본 요청이 보내지는 것을 막음 -> 페이지 다시 로드 되지 x
+
+        // 입력된 모든 데이터를 결합하는 객체
+        // enteredTitle, enteredAmoung, enteredDate 는 위에 작성된 상태변수를 가리킨다.
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData);
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
