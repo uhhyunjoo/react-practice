244a496acbad1d45e3cd652c02efc3f7a5804144
Hyunjoo Lee <alro92333@gmail.com>
2022-08-31 15:41:54 +0900

    🚚 Rename height to amount

--- demo-project/src/components/Expenses/Expenses.js
+++ demo-project/src/components/Expenses/Expenses.js
@@ -2,15 +2,15 @@ import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 
-function Expenses(props){
+function Expenses(props) {
     return (
-        <Card className = 'expenses'>
-            <ExpenseItem title={props.items[0].title} height={props.items[0].height} date={props.items[0].date}></ExpenseItem>
-            <ExpenseItem title={props.items[1].title} height={props.items[1].height} date={props.items[1].date}></ExpenseItem>
-            <ExpenseItem title={props.items[2].title} height={props.items[2].height} date={props.items[2].date}></ExpenseItem>
-            <ExpenseItem title={props.items[3].title} height={props.items[3].height} date={props.items[3].date}></ExpenseItem>
-            <ExpenseItem title={props.items[4].title} height={props.items[4].height} date={props.items[4].date}></ExpenseItem>
-            <ExpenseItem title={props.items[5].title} height={props.items[5].height} date={props.items[5].date}></ExpenseItem>
+        <Card className='expenses'>
+            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
+            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+            <ExpenseItem title={props.items[4].title} amount={props.items[4].amount} date={props.items[4].date}></ExpenseItem>
+            <ExpenseItem title={props.items[5].title} amount={props.items[5].amount} date={props.items[5].date}></ExpenseItem>
         </Card>
     );
 }
