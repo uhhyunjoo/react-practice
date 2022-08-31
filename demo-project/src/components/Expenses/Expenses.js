2c6706eac334b6110a6513e29287b893fdd5cb26
Hyunjoo Lee <alro92333@gmail.com>
2022-08-31 20:41:39 +0900

    🔨 Add ExpenseFilter (two-way binding)

--- demo-project/src/components/Expenses/Expenses.js
+++ demo-project/src/components/Expenses/Expenses.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
 
-    const saveFilterOptionHandler = (enteredFilterOption) => {
-        const filterOption = {
-            ...enteredFilterOption,
-            id: Math.random().toString()
-        };
-        console.log(filterOption);
+    const filterChangeHandler = (selectedYear) => {
+        console.log(selectedYear);
+        setFilteredYear(selectedYear);
     };
 
-
     return (
-        <Card className='expenses'>
-            <ExpensesFilter onChange={saveFilterOptionHandler}></ExpensesFilter>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
+                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+                <ExpenseItem title={props.items[4].title} amount={props.items[4].amount} date={props.items[4].date}></ExpenseItem>
+                <ExpenseItem title={props.items[5].title} amount={props.items[5].amount} date={props.items[5].date}></ExpenseItem>
+            </Card>
 
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
-            <ExpenseItem title={props.items[4].title} amount={props.items[4].amount} date={props.items[4].date}></ExpenseItem>
-            <ExpenseItem title={props.items[5].title} amount={props.items[5].amount} date={props.items[5].date}></ExpenseItem>
-        </Card>
+
+        </div>
+
     );
 }
 export default Expenses;
--- demo-project/src/components/Expenses/ExpensesFilter.js
+++ demo-project/src/components/Expenses/ExpensesFilter.js
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-    const [enteredOption, setEnteredOption] = useState('default');
 
     const optionChangeHandler = (event) => {
-        event.preventDefault();
-        //console.log('before:', enteredOption);
-        const expenseOption = {
-            option: event.target.value,
-        }
-
-        //console.log('after:', expenseOption);
-        props.onChange(expenseOption);
-        setEnteredOption(expenseOption);
-
+        props.onChangeFilter(event.target.value);
     };
 
     return (
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select onChange={optionChangeHandler} value={enteredOption} >
+                <select value={props.selected} onChange={optionChangeHandler} >
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
