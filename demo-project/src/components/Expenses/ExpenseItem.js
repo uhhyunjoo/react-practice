f6ca92788d330c91d19ef04a0428ec36f4ea2b56
Hyunjoo Lee <alro92333@gmail.com>
2022-11-01 17:52:59 +0900

    Update ExpenseItem to use props.title

--- demo-project/src/components/Expenses/ExpenseItem.js
+++ demo-project/src/components/Expenses/ExpenseItem.js
@@ -19,14 +19,12 @@ function ExpenseItem(props) {
 
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={props.date}></ExpenseDate>
+            <ExpenseDate date={props.date} />
 
             <div className='expense_-item__description'>
-                <h2> {title} </h2>
+                <h2> {props.title} </h2>
                 <div className='expense-item__price'>{props.amount}cm</div>
             </div>
-
-            <button onClick={clickHandler}>Change Title</button>
 
         </Card>
     );
