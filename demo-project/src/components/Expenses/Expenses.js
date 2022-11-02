30dba04c9517a7c5130018d8a956da3d233f133e
Hyunjoo Lee <alro92333@gmail.com>
2022-11-02 18:48:11 +0900

    Update condition feature

--- demo-project/src/components/Expenses/Expenses.js
+++ demo-project/src/components/Expenses/Expenses.js
@@ -21,9 +21,13 @@ function Expenses(props) {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-                {filteredExpenses.length === 0 ? <p>No expenses found.</p> : filteredExpenses.map((expense) => (
-                    <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-                ))}
+                {filteredExpenses.length === 0 &&
+                    <p>No expenses found.</p>}
+
+                {filteredExpenses.length > 0 &&
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+                    ))}
 
             </Card>
         </div>
