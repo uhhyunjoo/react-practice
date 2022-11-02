f1f7c834dfc261b1158e56e80891effc54413203
Hyunjoo Lee <alro92333@gmail.com>
2022-11-02 18:54:36 +0900

    Update condition feature shorter

--- demo-project/src/components/Expenses/Expenses.js
+++ demo-project/src/components/Expenses/Expenses.js
@@ -17,17 +17,20 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear;
     }); // return True or False
 
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+        ));
+    }
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-                {filteredExpenses.length === 0 &&
-                    <p>No expenses found.</p>}
 
-                {filteredExpenses.length > 0 &&
-                    filteredExpenses.map((expense) => (
-                        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-                    ))}
+                {expensesContent}
 
             </Card>
         </div>
