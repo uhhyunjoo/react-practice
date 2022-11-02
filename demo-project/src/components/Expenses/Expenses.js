a20b820e615f61417d781b5ee5ac522de1741118
Hyunjoo Lee <alro92333@gmail.com>
2022-11-02 18:22:30 +0900

    Add filter feature

--- demo-project/src/components/Expenses/Expenses.js
+++ demo-project/src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+    console.log(props);
     const [filteredYear, setFilteredYear] = useState('2020');
 
     const filterChangeHandler = (selectedYear) => {
@@ -12,13 +13,18 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    }); // return True or False
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
 
-                {props.items.map((expense) => (
-                    <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+                {filteredExpenses.map((expense) => (
+                    <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+
                 ))}
 
             </Card>
