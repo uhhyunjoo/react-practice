8eb3581fc0bde6744cf9aef9ffc219abdceb03c3
Hyunjoo Lee <alro92333@gmail.com>
2022-11-03 16:24:12 +0900

    Add condition feature (stopEditingHandler)

--- demo-project/src/components/NewExpense/NewExpense.js
+++ demo-project/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css';
 
 // 사용자 입력을 위한 form 을 반환하는 컴포넌트
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -11,10 +13,24 @@ const NewExpense = (props) => {
         };
         //console.log(expenseData);
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler}> Add New Expense</button>}
+            {isEditing &&
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler} />}
         </div>
     );
 };
