64cbfc7d633f07170b9e8feefcc17db35a4bda8e
Hyunjoo Lee <alro92333@gmail.com>
2022-11-03 17:05:33 +0900

    Add ExpenseList

--- demo-project/src/components/Expenses/ExpenseItem.js
+++ demo-project/src/components/Expenses/ExpenseItem.js
@@ -4,29 +4,27 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
-// Expense Item 데이터를 렌더링하는 컴포넌트
-// Keep in mind : Your componenet is a 'function' which returns 'JSX'
-
 function ExpenseItem(props) {
 
     const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
-        setTitle(props.title); // Update 누르면 제목 바뀜
+        setTitle(props.title);
         //console.log(title);
-
     };
 
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
 
-            <div className='expense_-item__description'>
-                <h2> {props.title} </h2>
-                <div className='expense-item__price'>{props.amount}cm</div>
-            </div>
+                <div className='expense_-item__description'>
+                    <h2> {props.title} </h2>
+                    <div className='expense-item__price'>{props.amount}cm</div>
+                </div>
 
-        </Card>
+            </Card>
+        </li>
     );
 }
 
--- demo-project/src/components/Expenses/Expenses.js
+++ demo-project/src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './Expenses.css'
-import ExpenseItem from "./ExpenseItem";
+
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
     console.log(props);
@@ -17,21 +18,16 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear;
     }); // return True or False
 
-    let expensesContent = <p>No expenses found.</p>;
 
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ));
-    }
 
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler} />
 
-                {expensesContent}
-
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
 
