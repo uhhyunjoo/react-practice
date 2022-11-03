285eac82a2931c2414c9919ef667c3a6c47269d3
Hyunjoo Lee <alro92333@gmail.com>
2022-11-03 17:59:59 +0900

    Update default values

--- demo-project/src/App.js
+++ demo-project/src/App.js
@@ -6,38 +6,38 @@ const DUMMY_EXPENSES = [
   {
     id: 'e1',
     title: '아이네',
-    amount: 158.00,
-    date: new Date(1994, 11, 17),
+    amount: 1.58,
+    date: new Date(2020, 11, 17),
   },
   {
     id: 'e2',
     title: '징버거',
-    amount: 161.9,
-    date: new Date(1995, 9, 8)
+    amount: 16.19,
+    date: new Date(2020, 9, 8)
   },
   {
     id: 'e3',
     title: '릴파',
     amount: 164,
-    date: new Date(1996, 2, 9),
+    date: new Date(2021, 2, 9),
   },
   {
     id: 'e4',
     title: '주르르',
-    amount: 161.9,
-    date: new Date(1997, 5, 10),
+    amount: 16.19,
+    date: new Date(2021, 5, 10),
   },
   {
     id: 'e5',
     title: '고세구',
-    amount: 30000,
-    date: new Date(1998, 8, 9),
+    amount: 30.00,
+    date: new Date(2022, 8, 9),
   },
   {
     id: 'e6',
     title: '비챤',
-    amount: 160,
-    date: new Date(2000, 0, 16),
+    amount: 16.0,
+    date: new Date(2022, 0, 16),
   },
 ];
 
